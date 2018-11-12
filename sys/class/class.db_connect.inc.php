ce36cec5093890203e3b900ebf6264de05e1e6fd
nt3goge <nt3goge@gmail.com>
2018-11-12 22:12:41 +0700

    init

---
+++ sys/class/class.db_connect.inc.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+class DBConnect 
+{
+    protected $db;
+
+    protected function __contruct($db = NULL)
+    {
+        if (is_object($db)) {
+            $this->db = $db;
+        } else {
+            $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+
+            try {
+                $this->db = new PDO($dsn, DB_USER, DB_PASS);
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
+        }
+    }
+}
+?>
